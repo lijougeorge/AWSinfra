1f8b14f12950c94260e314503a7dc8f98ee7aa24
lijo2george <lijo.2.george@atos.net>
2025-04-07 22:50:11 +0530

    updated federated notation

--- modules/eks/add_ons.tf
+++ modules/eks/add_ons.tf
@@ -1,6 +1,21 @@
+# Get cluster info using the cluster_name variable
+data "aws_eks_cluster" "eks_cluster" {
+  name = var.cluster_name
+}
+
+# Get OIDC provider URL from the UAT cluster
+data "aws_iam_openid_connect_provider" "oidc" {
+  url = data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
+# Local value to hold the stripped OIDC URL
+locals {
+  oidc_url = replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")
+}
+
 # Create IAM role for EFS CSI Driver
 resource "aws_iam_role" "efs_csi_driver" {
-  name = "${aws_eks_cluster.eks.name}-efs-csi-driver-role"
+  name = "${var.cluster_name}-efs-csi-driver-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -8,13 +23,12 @@ resource "aws_iam_role" "efs_csi_driver" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = "arn:aws:iam::${var.Account_ID}:oidc-provider/${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}"
+          Federated = "arn:aws:iam::${var.Account_ID}:oidc-provider/${local.oidc_url}"
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-            "${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:efs-csi-controller-sa",
-			"${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub" = "sts.amazonaws.com"
+            "${local.oidc_url}:sub" = "system:serviceaccount:kube-system:efs-csi-controller-sa"
           }
         }
       }
@@ -30,15 +44,14 @@ resource "aws_iam_role_policy_attachment" "efs_csi_driver_policy" {
 
 # EFS CSI Addon
 resource "aws_eks_addon" "aws_efs_csi_driver" {
-  cluster_name             = aws_eks_cluster.eks.name
+  cluster_name             = var.cluster_name
   addon_name               = "aws-efs-csi-driver"
   service_account_role_arn = aws_iam_role.efs_csi_driver.arn
 }
 
-
 # IAM Role for EBS CSI Driver
 resource "aws_iam_role" "ebs_csi_driver" {
-  name = "${aws_eks_cluster.eks.name}-ebs-csi-driver-role"
+  name = "${var.cluster_name}-ebs-csi-driver-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -46,13 +59,12 @@ resource "aws_iam_role" "ebs_csi_driver" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = "arn:aws:iam::${var.Account_ID}:oidc-provider/${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}"
+          Federated = "arn:aws:iam::${var.Account_ID}:oidc-provider/${local.oidc_url}"
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-            "${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:efs-csi-controller-sa",
-			"${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub" = "sts.amazonaws.com"
+            "${local.oidc_url}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
           }
         }
       }
@@ -68,24 +80,24 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
 
 # EBS CSI Addon
 resource "aws_eks_addon" "aws_ebs_csi_driver" {
-  cluster_name             = aws_eks_cluster.eks.name
+  cluster_name             = var.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 }
 
-
+# Core EKS Addons
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name = var.cluster_name
   addon_name   = "vpc-cni"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name = var.cluster_name
   addon_name   = "coredns"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name = var.cluster_name
   addon_name   = "kube-proxy"
 }
